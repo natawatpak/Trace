8bf8c1cb7a80ab1e58bab33192d886e37a1f4c4d
grostaco <grostaco@gmail.com>
2020-10-27 13:21:26 +0000

    All functions are now working

--- src/video/frames.c
+++ src/video/frames.c
@@ -7,7 +7,6 @@
 
 #include "cleanup.h"
 #include "video/frames.h"
-#include "image/imageutil.h"
 
 /*
  The OOP nature of this code is heavily inspired by glibc FILE 
@@ -24,6 +23,7 @@ static char error_buf[error_buf_len];
 #define error_buf self->_err_buf
 #endif
 
+
 // glibc's from libc-symbols.h
 #define weak_alias(name, aliasname) _weak_alias(name, aliasname)
 #define _weak_alias(name, aliasname)      \
@@ -34,7 +34,7 @@ static char error_buf[error_buf_len];
 #define attribute_hidden __attribute__((visibility("hidden")))
 
 extern __frame_frameobject* _frame_open(char*) attribute_hidden;
-extern int                  _frame_extract(__frame_frameobject*, enum AVPixelFormat, ...) attribute_hidden;
+extern int                  _frame_extract(__frame_frameobject*, enum AVPixelFormat, enum AVCodecID, char*, ...) attribute_hidden;
 extern int                  _frame_error(__frame_frameobject*) attribute_hidden;
 extern size_t               _frame_skip(__frame_frameobject*, size_t) attribute_hidden;
 extern int                  _frame_close(__frame_frameobject*) attribute_hidden;
@@ -66,8 +66,8 @@ __frame_frameobject* _frame_open(char *file) {
         return NULL;
     }
 
-    for (self->_video_index = 0 ; (unsigned)self->_video_index < self->_fCtx->nb_streams && self->_fCtx->streams[self->_video_index] == AVMEDIA_TYPE_VIDEO ; ++self->_video_index);
-    if (self->_fCtx->streams[self->_video_index] != AVMEDIA_TYPE_VIDEO) {
+    for (self->_video_index = 0 ; (unsigned)self->_video_index < self->_fCtx->nb_streams && self->_fCtx->streams[self->_video_index]->codecpar->codec_type != AVMEDIA_TYPE_VIDEO ; ++self->_video_index);
+    if (self->_fCtx->streams[self->_video_index]->codecpar->codec_type != AVMEDIA_TYPE_VIDEO) {
         snprintf(error_buf, 128, "__frame_extract_internal: cannot find video index for media");
         return NULL;
     }
@@ -91,35 +91,30 @@ __frame_frameobject* _frame_open(char *file) {
 
     self->_frame_rate = av_guess_frame_rate(self->_fCtx, self->_fCtx->streams[self->_video_index], NULL);
     self->_nbytes = av_image_get_buffer_size(AV_PIX_FMT_YUV420P, self->_cCtx->width, self->_cCtx->height, 16);
-    self->_underlying_buf = av_malloc(self->_nbytes);
+    self->_underlying_buf = av_malloc(self->_nbytes) ;
 
     av_image_fill_arrays(self->_frameRGB->data, self->_frameRGB->linesize, (uint8_t*)self->_underlying_buf, AV_PIX_FMT_YUV420P, self->_cCtx->width, self->_cCtx->height, 16);
-    self->_img_convert_ctx = sws_getContext(self->_cCtx->width, self->_cCtx->height,
-                                            self->_cCtx->pix_fmt,
-                                            self->_cCtx->width, self->_cCtx->height,
-                                            self->_cCtx->pix_fmt, 
-                                            SWS_BICUBIC,
-                                            NULL, NULL, NULL);
-
     return self;
 }
 weak_alias(_frame_open, frame_open);
 
 struct __frame_extract_opt {
     uint64_t bulksave:1;
-    uint64_t nosave:1; 
+    uint64_t sepsave:1; 
+    uint64_t skip:1;
     char*    format;
     int      nframes_requested;
 };
 
-int _frame_extract(__frame_frameobject* self, enum AVPixelFormat pixfmt, ...) {
-    static void* dispatch_table[] = {&&flag_endarg, &&flag_nsave, &&flag_bulksave, &&flag_nosave};
+int _frame_extract(__frame_frameobject* self, enum AVPixelFormat pixfmt, enum AVCodecID codecID, char* format, ...) {
+    static void* dispatch_table[] = {&&flag_endarg, &&flag_nsave, &&flag_bulksave, &&flag_sepsave, &&flag_skip};
     #define DISPATCH() goto *dispatch_table[va_arg(ap, int)]
-    struct __frame_extract_opt extopt = {0};
-    extopt.nframes_requested=1; // at least 1 frame
+    // default sep save
+    struct __frame_extract_opt extopt = {.sepsave=1, .nframes_requested=1};
+
     va_list ap;
     
-    va_start(ap, pixfmt);
+    va_start(ap, format);
 
     DISPATCH();
     flag_nsave :
@@ -127,22 +122,109 @@ int _frame_extract(__frame_frameobject* self, enum AVPixelFormat pixfmt, ...) {
         DISPATCH();
     flag_bulksave:
         extopt.bulksave=1;
+        extopt.sepsave=0;
         DISPATCH();
-    flag_nosave :
-        extopt.nosave=1;
+    flag_sepsave :
+        extopt.sepsave=1;
+        extopt.bulksave=0;
+        DISPATCH();
+    flag_skip :
+        extopt.skip=1;
         DISPATCH();
     flag_endarg :
+        va_end(ap);
 
-    va_end(ap);
-
-    // if FRAME_NOSAVE were passed with other save options, it will be ignored
-    if (extopt.nosave && extopt.bulksave) {
-        extopt.nosave=0;
-    }else if (extopt.nosave && extopt.nframes_requested) {
-        extopt.nosave=0;
+    if (self->_img_convert_ctx == NULL && !extopt.skip) {
+        self->_img_convert_ctx = sws_getContext(self->_cCtx->width, self->_cCtx->height,
+                                            self->_cCtx->pix_fmt,
+                                            self->_cCtx->width, self->_cCtx->height,
+                                            pixfmt, 
+                                            SWS_BICUBIC,
+                                            NULL, NULL, NULL);
     }
 
-    
+    AVCodecContext* c = avcodec_alloc_context3(NULL);
+    AVCodec* codec = avcodec_find_encoder(codecID);    
+    AVPacket pkt;
+    FILE*    dst;
+    char     filename_buffer[256];
+
+    if (extopt.bulksave) {
+        dst = fopen(format, "w");
+    }
+
+    c->bit_rate  = self->_cCtx->bit_rate;
+    c->width     = self->_cCtx->width;
+    c->height    = self->_cCtx->height;
+    c->time_base = (AVRational){1,25};
+    c->pix_fmt   = AV_PIX_FMT_YUVJ420P;
+
+    self->_frameRGB->width  = self->_cCtx->width;
+    self->_frameRGB->height = self->_cCtx->height;
+    self->_frameRGB->format = c->pix_fmt;
+
+    avcodec_open2(c, codec, NULL);
+
+    while(extopt.nframes_requested && av_read_frame(self->_fCtx, &pkt) >= 0) {
+        if (pkt.stream_index != self->_video_index) {
+            continue;
+        }
+        if ((self->_errnum = avcodec_send_packet(self->_cCtx, &pkt)) < 0) {
+            av_strerror(self->_errnum, error_buf, error_buf_len);
+            fprintf(stderr, "avcodec_send_packet: %s\n", error_buf);
+            return self->_errnum;  
+        }
+        if ((self->_errnum = avcodec_receive_frame(self->_cCtx, self->_frame)) != 0) {
+            continue;
+        }
+
+        if (extopt.skip) {
+            extopt.nframes_requested--;
+            self->_cframe++;
+            continue;
+        }        
+
+        sws_scale(self->_img_convert_ctx, (const unsigned char* const*)self->_frame->data, self->_frame->linesize, 0, self->_cCtx->height, self->_frameRGB->data, self->_frameRGB->linesize);
+        
+        if ((self->_errnum = avcodec_send_frame(c, self->_frameRGB)) < 0) {
+            av_strerror(self->_errnum, error_buf, error_buf_len);
+            fprintf(stderr, "avcodec_send_frame: %s", error_buf);       
+            return self->_errnum;
+        }
+        if ((self->_errnum = avcodec_receive_packet(c, &pkt)) < 0) {
+            av_strerror(self->_errnum, error_buf, error_buf_len);
+            fprintf(stderr, "avcodec_receieve_packet: %s", error_buf);
+            return self->_errnum;
+        }
+        
+        if (extopt.bulksave) {
+            fwrite(&pkt.size, sizeof(pkt.size), 1, dst);
+            fwrite(pkt.data, pkt.size, 1, dst);
+        }else {
+            snprintf(filename_buffer, 256, format, self->_cframe/60, self->_cframe%60);
+            dst = fopen(filename_buffer, "w");
+            if (dst == NULL) {
+                perror("fopen");
+                return -errno;
+            }
+
+            fwrite(pkt.data, pkt.size, 1, dst);
+            fclose(dst);
+        }
+
+        // repeat until finished if nframes_requested is -1
+        if (extopt.nframes_requested != -1) {
+            extopt.nframes_requested--;
+        }
+        self->_cframe++;
+    }    
+
+    if (extopt.bulksave) {
+        fclose(dst);
+    }
+
+    avcodec_close(c);
+    avcodec_free_context(&c);
 
     return 0;
 }
@@ -155,171 +237,21 @@ int _frame_error(__frame_frameobject* self) {
 weak_alias(_frame_error, frame_error);
 
 size_t _frame_skip(__frame_frameobject* self, size_t nframes) {
-    (void)self;
-    (void)nframes;
+    _FRAME_EXTRACT(self, AV_PIX_FMT_RGB24, AV_CODEC_ID_MJPEG, "", FRAME_SKIP, FRAME_NSAVE, nframes, FRAME_ENDARG);  
     return 0;
 }
 weak_alias(_frame_skip, frame_skip);
 
 int _frame_close(__frame_frameobject* self) {
-    (void)self;
+    free(_FRAME_REINTERPRET_ACCESS(self, struct __frame_frameobject_plus, vtable));
+
+    av_free(self->_underlying_buf);
+    av_frame_free(&self->_frame);
+    av_frame_free(&self->_frameRGB);
+    avformat_free_context(self->_fCtx);
+    avcodec_free_context(&self->_cCtx);
+
+    free(self);
     return 0;
 }
 weak_alias(_frame_close, frame_close);
-
-/*
-static int __frame_extract_frames(const char* url, enum AVPixelFormat pixfmt, const char* dst, AVCodecContext* c) {
-    AVFormatContext* fCtx;
-    AVCodecContext*  cCtx = NULL;
-    AVCodec*         codec;
-    AVFrame          *frame, *frameRGB;
-    AVPacket         pkt;
-    char             errorBuffer[256], *buffer, nBuffer[64];
-    int              r;
-    unsigned int     vindex, nbytes;
-
-    fCtx = avformat_alloc_context();
-    if ((r = avformat_open_input(&fCtx, url, NULL, NULL)) < 0) {
-        av_strerror(r, errorBuffer, 256);
-        fprintf(stderr, "avformat_open_input: %s\n", errorBuffer);
-        return r; 
-    }
-
-    if ((r = avformat_find_stream_info(fCtx, NULL)) != 0) {
-        av_strerror(r, errorBuffer, 256);
-        fprintf(stderr, "avformat_find_stream_info: %s\n", errorBuffer);
-        return r; 
-    }
-
-
-
-#if DUMP_FORMAT
-    av_dump_format(fCtx, 0, url, 0);
-#endif
-
-    AVRational avfps = av_guess_frame_rate(fCtx, *fCtx->streams, NULL);
-    int        fps   = ceil((double)avfps.num/avfps.den);
-
-    VIDEO_INDEX(fCtx, vindex);
-
-    if (vindex > fCtx->nb_streams || fCtx->streams[vindex]->codecpar->codec_type!=AVMEDIA_TYPE_VIDEO) {
-        fprintf(stderr, "Cannot find video stream\n");
-        return r;
-    }
-
-    if (c != NULL) {
-        cCtx = c;
-    } else {
-        cCtx = avcodec_alloc_context3(NULL);
-    }
-
-    avcodec_parameters_to_context(cCtx, fCtx->streams[vindex]->codecpar);
-
-    if ((codec = avcodec_find_decoder(cCtx->codec_id)) == NULL) {
-        fprintf(stderr, "Cannot find decoder for codec id: %d\n", cCtx->codec_id);
-        return r;
-    }
-
-    if ((r = avcodec_open2(cCtx, codec, NULL)) != 0) {
-        av_strerror(r, errorBuffer, 256);
-        fprintf(stderr, "avcodec_open2: %s\n", errorBuffer);
-        return r; 
-    }
-
-    frame = av_frame_alloc();
-    frameRGB = av_frame_alloc();
-    if (!frame || !frameRGB) {
-        av_strerror(r, errorBuffer, 256);
-        fprintf(stderr, "av_frame_alloc: %s\n", errorBuffer);
-        return r; 
-    }
-
-    nbytes = av_image_get_buffer_size(pixfmt, cCtx->width, cCtx->height, 16);
-    buffer = av_malloc(nbytes);
-
-    av_image_fill_arrays(frameRGB->data, frameRGB->linesize, (uint8_t*)buffer, pixfmt, cCtx->width, cCtx->height, 16);
-    
-    static struct SwsContext* img_convert_ctx;
-    if (img_convert_ctx == NULL) {
-        img_convert_ctx =sws_getContext(cCtx->width, cCtx->height,
-                       cCtx->pix_fmt,
-                       cCtx->width, cCtx->height,
-                       pixfmt,
-                       SWS_BICUBIC,
-                       NULL, NULL, NULL);
-    }
-
-    int i = 0;
-    while (av_read_frame(fCtx, &pkt) >= 0) {
-        if ((unsigned)pkt.stream_index == vindex) {
-            avcodec_send_packet(cCtx, &pkt);
-            r = avcodec_receive_frame(cCtx, frame);
-            if (r == 0) {
-                r = sws_scale(img_convert_ctx, (const unsigned char* const*)frame->data, frame->linesize, 0, cCtx->height, frameRGB->data, frameRGB->linesize);
-
-                snprintf(nBuffer, 64, dst, i / fps, i % fps);
-                saveFrame(cCtx, frameRGB, AV_CODEC_ID_MJPEG, nBuffer);
-                i++;
-#ifdef FIRST_FRAME_ONLY
-                break;
-#endif
-            }
-        }
-    }
-
-    avformat_free_context(fCtx);
-    if (c == NULL) {
-        avcodec_free_context(&cCtx);
-    }
-    av_frame_free(&frame);
-    av_frame_free(&frameRGB);
-    av_free(buffer);
-    
-
-    return 0;
-}
-
-static int saveFrame(AVCodecContext* cCtx, AVFrame* frame, enum AVCodecID codecID, const char* dst) {
-    AVCodec*            enc = avcodec_find_encoder(codecID);
-    AVCodecContext*     c   = avcodec_alloc_context3(NULL);
-    AVPacket*           pkt;
-    int                 r;
-
-    pkt = av_packet_alloc();
-    c->bit_rate = cCtx->bit_rate;
-    c->width = cCtx->width;
-    c->height = cCtx->height;
-    c->time_base= (AVRational){1,25};
-    c->pix_fmt = AV_PIX_FMT_YUVJ420P;
-
-    frame->width = cCtx->width;
-    frame->height = cCtx->height;
-    frame->format=c->pix_fmt;
-
-    avcodec_open2(c, enc, NULL);
-
-    if ((r = avcodec_send_frame(c, frame)) < 0) {
-        return r;
-    }
-    if ((r = avcodec_receive_packet(c, pkt)) < 0) {
-        return r;
-    }
-    
-
-    FILE* fp = fopen(dst, "w");
-    if (fp == NULL) {
-        perror("fopen");
-        
-        return errno;
-    }
-
-    fwrite(pkt->data, 1, pkt->size, fp);
-    fclose(fp);
-
-    av_packet_free(&pkt);
-    avcodec_close(c);
-    avcodec_free_context(&c);
-    return 0;
-}
-
-*/
