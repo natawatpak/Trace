e37c611dd36579a31b12fe10b86d6908d5b0dc47
grostaco <grostaco@gmail.com>
2020-11-13 07:26:12 +0000

    added common.h

---
+++ include/common.h
@@ -0,0 +1,19 @@
+#ifndef __COMMON_H
+#define __COMMON_H
+
+// glibc's from libc-symbols.h
+#define weak_alias(name, aliasname) _weak_alias(name, aliasname)
+#define _weak_alias(name, aliasname)      \
+    extern __typeof__(name) aliasname     \
+    __attribute__((weak, alias(#name)))   \
+    __attribute_copy__(name)                                      
+
+#define DEFINE_TRIVIAL_LINKED_LIST(name, dt, dtname) \
+    struct name {          \
+        dt dtname;         \
+        struct name* next; \
+    };
+
+#define attribute_hidden __attribute__((visibility("hidden")))
+
+#endif
