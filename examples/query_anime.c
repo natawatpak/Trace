3d60806a83b94f55b33aef1a45050fc7e6b92044
grostaco <grostaco@gmail.com>
2020-11-14 11:13:15 +0000

    examples edit

--- examples/query_anime.c
+++ examples/query_anime.c
@@ -18,10 +18,11 @@ int main (int argc, char** argv) {
     QueryObject* qobj = query_new(argv[1], argv[2], argv[3], argv[4]);
     query_establish_connection(qobj);
     fprintf(stdout, "Session ID: %s\n", query_refresh_session(qobj));
-    anidb_response ares = query_by_id(qobj, 1, "b2f0e0fc000000");
+    anidb_response ares = query_by_id(qobj, 1, query_int_to_amask(QUERY_FLAG_AWARD_LIST | QUERY_FLAG_AID | QUERY_FLAG_VOTE_CNT | QUERY_FLAG_IS_NSFW));
     (void)ares;
     query_anidb_response_free(&ares);
     query_free(qobj);
+    
     
     return 0;
     
