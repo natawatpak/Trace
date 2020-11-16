2edd797fb0500b07f85d9e1f51edac40b440eef8
grostaco <grostaco@gmail.com>
2020-11-16 20:00:12 +0000

    added MAX_SESSION_LEN

--- include/config.h
+++ include/config.h
@@ -6,13 +6,14 @@
 #define NO_SILENT_ERROR
 
 /* query configs */
-#define ANIDB_ENDPOINT "api.anidb.net"
-#define ANIDB_PORT     "9000"
-#define ANIDB_PROTOVER "3"
-#define ANIDB_NREAD    1024
-#define ANIDB_AUTHFMT  "AUTH user=%s&pass=%s&clientver=%s&client=%s&protover="ANIDB_PROTOVER
-#define ANIDB_ANAMEFMT "ANIME s=%s&aname=%s&amask=%s"
-#define ANIDB_ANIIDFMT "ANIME s=%s&aid=%d&amask=%s"
-#define ANIDB_NSESSION 5
+#define ANIDB_ENDPOINT  "api.anidb.net"
+#define ANIDB_PORT      "9000"
+#define ANIDB_PROTOVER  "3"
+#define ANIDB_NREAD     1024
+#define ANIDB_AUTHFMT   "AUTH user=%s&pass=%s&clientver=%s&client=%s&protover="ANIDB_PROTOVER
+#define ANIDB_ANAMEFMT  "ANIME s=%s&aname=%s&amask=%s"
+#define ANIDB_ANIIDFMT  "ANIME s=%s&aid=%d&amask=%s"
+#define ANIDB_NSESSION  5
+#define MAX_SESSION_LEN 32
 
 #endif
