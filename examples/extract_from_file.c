eb6c8556c766db18a77b58d36833cc72021e345e
grostaco <grostaco@gmail.com>
2020-11-17 03:24:18 +0000

    minor extract bug fixes

--- examples/extract_from_file.c
+++ examples/extract_from_file.c
@@ -12,6 +12,13 @@ static int last_found(char* path, int c) {
     return index;
 }
 
+static int explicit_last_found(char* path, int c, int n) {
+    int index = -1;
+    if (n < 0)return index;
+    for (int i = 0 ; i < n ; ++i) if (path[i] == c)index = i;
+    return index;
+}
+
 int main (int argc, char** argv) { 
 
     int option_index;
@@ -114,9 +121,9 @@ int main (int argc, char** argv) {
                                                                               QUERY_FLAG_RELATED_AID_TYPE | QUERY_FLAG_ROMANJI_NAME | QUERY_FLAG_ENGLISH_NAME  |\
                                                                               QUERY_FLAG_EPISODES | QUERY_FLAG_RATINGS | QUERY_FLAG_TAG_NAME_LIST));
 
-        int i = last_found(outfile, '/');
+        int i = explicit_last_found(outfile, '/', last_found(outfile, '/'));
         if (i != -1)outfile[i]='\0';
-        snprintf(buffer, 256, "%s/metadata.json", outfile, btmp);
+        snprintf(buffer, 256, "%s/metadata.json", outfile);
         FILE* json = fopen(buffer, "w");
         if (i != -1)outfile[i]='/';
         fprintf(json, "{\
@@ -129,8 +136,7 @@ int main (int argc, char** argv) {
                        \n\t\"QUERY_FLAG_ENGLISH_NAME\":\"%s\",\
                        \n\t\"QUERY_FLAG_EPISODES\":\"%d\",\
                        \n\t\"QUERY_FLAG_RATINGS\":\"%d\",\
-                       \n\t\"QUERY_FLAG_TAG_NAME_LIST\":\"%s\"\
-                       }",
+                       \n\t\"QUERY_FLAG_TAG_NAME_LIST\":\"%s\" \n}",
                        ani_res->aid, ani_res->year.from, ani_res->year.to,
                        ani_res->type, query_any_list_concat((struct any_list*)ani_res->related_aid_list, ","), query_any_list_concat((struct any_list*)ani_res->related_aid_type, ","),
                        ani_res->romanji_name, ani_res->english_name, 
